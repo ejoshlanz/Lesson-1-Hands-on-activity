8a4f4224c136190cf40c2ad9ad1c6299e08aa61c
Joshua <ejoshlanzuela@gmail.com>
2025-02-23 00:06:16 +0800

    Initial commit on lesson 1 hands on activity

---
+++ script.js
@@ -0,0 +1,8 @@
+// The given sample JavaScript code
+console.log("Hello, World!");
+let name = "Student";
+console.log("Welcome, " + name);
+
+// I still don't get what it means by linking it, but here's the output linked to the HTML file
+document.getElementById("output1").innerHTML = "Hello, World!";
+document.getElementById("output2").innerHTML = "Welcome, " + name;
